4da6e6d6c4acd531b753bc7deea6ac51bac88f20
Jan Pieter Waagmeester <jieter@jieter.nl>
2016-02-19 12:04:52 +0100

    Initial import

---
+++ example/example.js
@@ -0,0 +1,41 @@
+var map = L.map('map', {
+    zoomControl: false,
+    center: [48, -3],
+    zoom: 5
+});
+
+var layers = {
+    'OSM': L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }),
+
+    'Toner': L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20
+    }),
+
+    'Watercolor': L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        subdomains: 'abcd',
+        minZoom: 3,
+        maxZoom: 16
+    })
+};
+
+var layersControl = L.control.layers.buttons(layers).addTo(map);
+
+layers.OSM.addTo(map);
+
+var info = L.control();
+info.options.position = 'topleft';
+info.onAdd = function () {
+    var div = L.DomUtil.create('div', 'info');
+    L.DomUtil.create('h4', '', div).innerHTML = '<a href="https://github.com/jieter/Leaflet.layerscontrol-buttons">Leaflet.layerscontrol-buttons preview</a>';
+    L.DomUtil.create('p', '', div).innerHTML = 'Leaflet layers control buttons.';
+
+    return div;
+};
+
+info.addTo(map);
